55f49ed6311e8fdd6942e0c72341895eb1bd8b3c
sinwolfe <mqhakimi@pm.me>
2025-09-25 02:47:31 +0800

    about: add header

--- bridges/src/routes/about/+page.svelte
+++ bridges/src/routes/about/+page.svelte
@@ -3,3 +3,14 @@
 
 	const { contributors } = data;
 </script>
+
+<svelte:head>
+	<title>about - bridges</title>
+</svelte:head>
+
+<main>
+	<header>
+		<h1>bridges</h1>
+		<div class="muted">a minimal file sharing utility</div>
+	</header>
+</main>
