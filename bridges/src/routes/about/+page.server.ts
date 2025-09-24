54e9c1d5be2ad5a242a1880485153821cf05302a
sinwolfe <mqhakimi@pm.me>
2025-09-25 02:17:52 +0800

    about: sinwolfe contrib

---
+++ bridges/src/routes/about/+page.server.ts
@@ -0,0 +1,12 @@
+export function load() {
+	return {
+		contributors: [
+			{
+				name: 'sinwolfe',
+				url: 'https://github.com/sinwolfe',
+				notes:
+					'Set up the project, designed branding, designed all the UI in figma, implemented login, registration functionality, scaffolded both admin and user dashboard'
+			}
+		]
+	};
+}
