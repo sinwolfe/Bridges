a187b2281fbd1ab7442e6efc232e14762d15bb73
sinwolfe <mqhakimi@pm.me>
2025-09-26 23:40:39 +0800

    chore: proper env for pb

--- bridges/src/lib/pocketbase.ts
+++ bridges/src/lib/pocketbase.ts
@@ -1,6 +1,7 @@
 import PocketBase from 'pocketbase';
+import { PB_URL } from '$env/static/private';
 
-export const pb = new PocketBase("http://127.0.0.1:8090");
+export const pb = new PocketBase(PB_URL);
 export default pb;
 
 // Only run this in the browser
