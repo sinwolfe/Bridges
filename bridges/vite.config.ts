261ea14e5bf5fa8e983c7039f922acb6e17705b6
sinwolfe <mqhakimi@pm.me>
2025-09-29 17:18:35 +0800

    chore: add allowedHost for production

--- bridges/vite.config.ts
+++ bridges/vite.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	server: {
+		allowedHosts: ['putyourhosthere']
+	},
 	test: {
 		expect: { requireAssertions: true },
 		projects: [
